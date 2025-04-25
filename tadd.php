9e86e0f978cf8b20cafb7f8c6116cbfe86c7b5c9
Tadd-company <Ifanatsempai@gmail.com>
2025-04-25 21:09:14 +0300

    Update tadd.php

--- tadd.php
+++ tadd.php
@@ -1,11 +1,1 @@
-<?php
-$pol=mysqli_connect("localhost","root","","tadd") or die ('Brak poloczenia z serwerem MySQL');
-$fullname=$_POST['fullname'];
-$email=$_POST['email'];
-$phonenumber=$_POST['phonenumber'];
-$services=$_POST['services'];
-$message=$_POST['message'];
-$dodaj_dane=mysqli_query($pol, "INSERT INTO dane (fullname,email,phonenumber,services,message) value ('$fullname','$email','$phonenumber','$services','$message')");
-header("Location: tadd.html");
-exit
-?>
+
